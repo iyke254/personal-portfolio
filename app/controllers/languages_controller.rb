5838d9486655fb16690eda23e6c1280665d79809
iyke254 <nderiony95@gmail.com>
2017-08-04 13:00:32 +0300

    fix bugs

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,6 +1,10 @@
-class LanguagesController < Applicationcontroller
+class LanguagesController < ApplicationController
 	def index
 		@languages = Language.all
 		render :index
 	end
+
+	def show
+		@languages = Language.find(params[:id])
+		render :show
 end		
