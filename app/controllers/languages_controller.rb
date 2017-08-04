56250f1dfd1bdfb85622c9e77b10c28509cf6871
iyke254 <nderiony95@gmail.com>
2017-08-04 14:20:00 +0300

    add the new route functionality

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,4 +1,9 @@
 class LanguagesController < ApplicationController
+	def new
+		@language = Language.new
+		render :new
+	end
+
 	def index
 		@languages = Language.all
 		render :index
@@ -8,4 +13,18 @@ class LanguagesController < ApplicationController
 		@language = Language.find(params[:id])
 		render :show
 	end
+
+	def create
+		@language = Language.new(language_params)
+		if @language.save
+			redirect_to languages_path
+		else
+			render :new
+		end
+	end
+
+	private
+		def language_params
+			params.require(:list).permit(:name)	
+		end			
 end		
