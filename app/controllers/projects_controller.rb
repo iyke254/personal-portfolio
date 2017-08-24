ee8afaa131d28a5c01b275dc7abde0d14c9b8d8e
iyke254 <nderiony95@gmail.com>
2017-08-24 10:27:20 +0300

    fix errors on the languages and projects path

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,31 @@ class ProjectsController < ApplicationController
 		end
 	end	
 
+	def edit
+   		@language = List.find(params[:language_id])
+   		@task = @language.tasks.find(params[:id])
+   
+ 	end
+
+	def update
+  		 @language = List.find(params[:language_id])
+  		 @task = @language.tasks.find(params[:id])
+   	if @task.update(task_params)
+     	flash[:notice] = "Task successfully updated!"
+    	redirect_to language_path(@language)
+    else
+     	render :edit
+   	end
+ end
+
+def destroy
+   @language = List.find(params[:language_id])
+   @task = @language.tasks.find(params[:id])
+   @task.destroy
+   flash[:notice] = "Task successfully deleted!"
+   redirect_to language_path(@language)
+ end
+
 	private
     	def project_params
       		params.require(:project).permit(:description)
